4b7010a8f837b4c4dc62ed4e4b7899bcaf769638
Jeffrey DeYoung <Jeffrey.J.DeYoung@gmail.com>
2015-12-31 10:34:47 -0700

    see last commit; forgot one

---
+++ src/test/java/com/docussandra/javasdk/dao/impl/QueryDaoImplTest.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.docussandra.javasdk.dao.impl;
+
+import com.docussandra.javasdk.Config;
+import com.docussandra.javasdk.dao.QueryDao;
+import com.docussandra.javasdk.testhelper.TestUtils;
+import com.strategicgains.docussandra.domain.objects.Query;
+import com.strategicgains.docussandra.domain.objects.QueryResponseWrapper;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Ignore;
+
+/**
+ *
+ * @author jeffrey
+ */
+public class QueryDaoImplTest
+{
+    
+    private Config config;
+    private QueryDao instance;
+
+    public QueryDaoImplTest()
+    {
+        config = new Config("http://localhost:8081/", Config.Format.SHORT);
+        instance = new QueryDaoImpl(config);
+    }
+    
+    @BeforeClass
+    public static void setUpClass()
+    {
+    }
+    
+    @AfterClass
+    public static void tearDownClass()
+    {
+    }
+    
+    @Before
+    public void setUp() throws InterruptedException
+    {
+        TestUtils.insertTestDb(config);
+        TestUtils.insertTestTable(config);
+        TestUtils.insertTestIndex(config);
+        Thread.sleep(2000);
+    }
+
+    @After
+    public void tearDown() throws InterruptedException
+    {
+        TestUtils.cleanupTestDb(config);
+        Thread.sleep(1000);
+    }
+
+    /**
+     * Test of query method, of class QueryDaoImpl.
+     */
+    @Test
+    public void testQuery_String_Query() throws Exception
+    {
+        System.out.println("query");
+        String db = TestUtils.getTestDb().name();
+        Query query = new Query();
+        query.setTable(TestUtils.getTestTable().name());
+        query.setWhere("test = 'testvalue'");
+        QueryResponseWrapper result = instance.query(db, query);
+        assertNotNull(result);
+    }
+
+    /**
+     * Test of query method, of class QueryDaoImpl.
+     */
+    @Test
+    @Ignore
+    public void testQuery_4args() throws Exception
+    {
+        System.out.println("query");
+        String db = "";
+        Query query = null;
+        int limit = 0;
+        long offset = 0L;
+        QueryDaoImpl instance = null;
+        QueryResponseWrapper expResult = null;
+        QueryResponseWrapper result = instance.query(db, query, limit, offset);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
